8cc24a36207fc1b38b7b6572e5f542200a376c56
matheus-teko <106824106+matheus-teko@users.noreply.github.com>
2022-08-26 15:29:45 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+class Teixeira extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Olá, Turma!</h1>
+      </div>
+    );
+  }
+}
+export default Teixeira;
